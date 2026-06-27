d78dc59d24680c0a40f34e3721648b29244607fb
Chitrak Betal <chitrak14789@gmail.com>
2026-06-27 18:14:42 +0530

    Frontend Deploy

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getTabId, tokenKey } from './tabSession';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
